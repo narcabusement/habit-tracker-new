4f73a7f1edc12eaff4ed8127d9e617ccb4ca0fcc
narcabusement <narc.abusement@gmail.com>
2026-06-25 19:15:26 +0200

    Add service worker for caching assets

---
+++ Sw.js
@@ -0,0 +1,20 @@
+const CACHE_NAME = "habit-tracker-v1";
+const ASSETS = [
+  "./",
+  "./index.html",
+  "./styles.css",
+  "./app.js",
+  "./manifest.webmanifest"
+];
+
+self.addEventListener("install", (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS))
+  );
+});
+
+self.addEventListener("fetch", (event) => {
+  event.respondWith(
+    caches.match(event.request).then((cached) => cached || fetch(event.request))
+  );
+});
